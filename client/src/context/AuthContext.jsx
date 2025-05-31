649ff0b84a85d507f9ebc63141944883f00c3742
ShouryAG111 <shouryagi771@gmail.com>
2025-06-01 01:57:00 +0530

    new

--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -5,7 +5,7 @@ import axios from "axios"
 import { toast } from "react-toastify"
 
 
-axios.defaults.baseURL = process.env.REACT_APP_API_URL || "https://referhub.onrender.com"
+axios.defaults.baseURL = "https://referhub.onrender.com"
 
 const AuthContext = createContext()
 
